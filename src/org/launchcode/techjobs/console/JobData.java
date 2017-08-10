a6df72d566e0d014b1aea4bb5bc10822cf985d2a
Patrick Lippert <patrick.lippert1@gmail.com>
2017-08-10 16:41:39 -0700

    Created method findByValue.

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -84,6 +84,25 @@ public class JobData {
         return jobs;
     }
 
+    public static ArrayList<HashMap<String, String>> findByValue(String value){
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        //Load data, if it is not already loaded.
+        loadData();
+
+        for (HashMap<String, String> row : allJobs) {
+
+            for (String aValue : row.values()) {
+                if (aValue.contains(value)) {
+                    jobs.add(row);
+                }
+                break;
+            }
+        }
+
+        return jobs;
+    }
+
     /**
      * Read in data from a CSV file and store it in a list
      */
